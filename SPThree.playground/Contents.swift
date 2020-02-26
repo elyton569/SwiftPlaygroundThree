09bd5a9a34ff1a24cbba28e2516e6c46ebdb097f
Oliver Corpuz <ocorpuz@hawaii.edu>
2020-02-25 16:09:30 -1000

    finished intro to arrays

--- SPThree.playground/Contents.swift
+++ SPThree.playground/Contents.swift
@@ -38,3 +38,40 @@ for name in fruitNames{
 print(fruitNames[1])
 
 //print item at index 0
+
+
+// var ExpensiveCars : [String]
+
+//Problem Set #1 Part 1
+
+var ExpensiveCars = ["Zenvo", "Ferrari", "Pagani Huayra", "Aston Martin One", "Koenigesegg One", "Ferrari F60 Amercia", "Lamborghini Veneno", "Tesla Roadster" ]
+
+print(ExpensiveCars)
+
+
+
+/*Desried result
+ I like expensive car Zenvo
+ I like expensive car Ferrari
+ I like expensive car Pagani Huayra
+ I like expensive car Aston Martin One
+ I like expensive car Koenigesgg One
+ I like expensive car Ferrari F60 Amercia
+ I like expensive car Lamborghini Veneno
+ I like expensive car Tesla Roadster
+
+ */
+
+print("I like cars \(ExpensiveCars)")
+
+
+//Problem Set #1 Part 2 in the loop
+ 
+ print("Item in array is a very expensive car")
+
+//print problem set#2 part 1
+
+print(ExpensiveCars[8])
+//print item at index 8
+
+print("Item in the array + 100")
